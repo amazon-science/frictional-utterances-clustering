8eaa84e6e0ca71023054af4528f69481ce477e53
anton87 <3440638+Anton87@users.noreply.github.com>
2024-01-11 12:52:37 +0100

    Added script to download base language models

---
+++ src/frictional_utterances_clustering/downlad_base_sentence_encoders.py
@@ -0,0 +1,58 @@
+import os
+from sentence_transformers import SentenceTransformer
+from transformers import BertModel, BertTokenizer, AutoTokenizer, AutoModelForMaskedLM
+
+# Get the absolute path of the directory where the Python script is located
+current_directory = os.path.dirname(os.path.abspath(__file__))
+
+# The GitHub project directory is one level above the script directory 'src'
+github_project_directory = os.path.dirname(os.path.dirname(current_directory))
+
+# Download and save locally the Paraphrase Multilingual Mpnet model
+
+def load_model(model_cls, model_name):
+    try:
+        model = model_cls(model_name)
+    except (ValueError, EnvironmentError):
+        model = model_cls.from_pretrained(model_name)
+    return model
+
+def save_model(model, tokenizer, model_path, ):
+    try:
+        model.save(model_path)
+    except AttributeError:
+        model.save_pretrained(model_path)
+        tokenizer.save_pretrained(model_path)
+
+def download_and_save_model(tokenizer_cls, model_cls, model_name, model_dir, model_ns=None):
+    """
+    Downloads a model from Hugging Face using the specified tokenizer and model classes,
+    saves it locally
+     at the specified directory, and prints a message with a checkmark
+    emoji upon successful saving.
+
+    Args:
+    - tokenizer_cls (type): The tokenizer class (e.g., AutoTokenizer) to use for the model.
+    - model_cls (type): The model class (e.g., SentenceTransformer) to use for the model.
+    - model_name (str): The name of the model to download.
+    - model_dir (str): The directory path to save the downloaded model.
+    - model_namespace (str): The namespace of the model on Hugging Face.
+
+    """
+    model_path = os.path.join(model_dir, model_name)
+    model_path = f"{github_project_directory}/base_language_models/{model_name}"
+    print(f"Downloading the model '{model_name}' from Hugging Face... ", flush=True, end="")
+    full_model_name = f"{model_ns}/{model_name}" if model_ns is not None else model_name
+    tokenizer = tokenizer_cls.from_pretrained(full_model_name)
+    model = load_model(model_cls, f'{model_name}')
+    print("✅")
+    print(f"Saving the model at '{model_path}'... ", flush=True, end="")
+    save_model(model, tokenizer, model_path)
+    print("✅", end="\n\n")
+
+SAVED_MODEL_DIR = f"{github_project_directory}/base_language_models"
+
+download_and_save_model(AutoTokenizer, SentenceTransformer, "paraphrase-multilingual-mpnet-base-v2", SAVED_MODEL_DIR, "sentence-transformers")
+download_and_save_model(AutoTokenizer, SentenceTransformer, "paraphrase-mpnet-base-v2", SAVED_MODEL_DIR, "sentence-transformers")
+download_and_save_model(BertTokenizer, BertModel, "bert-base-multilingual-cased", SAVED_MODEL_DIR)
+download_and_save_model(AutoTokenizer, AutoModelForMaskedLM, "xlm-roberta-base", SAVED_MODEL_DIR)
